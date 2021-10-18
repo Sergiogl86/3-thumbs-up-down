e51a325745068a04721eecea71e39668a87a9034
Sergio Garcia <sergio.gl86@gmail.com>
2021-10-18 18:27:31 +0200

    Se completa 3-thumbs-up-down

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import Button from "./Component/Button";
+import FlipIcon from "./Component/FlipIcon";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [icono, setIcono] = useState("icono fa fa-thumbs-up");
+  const cambiarIcono = function () {
+    setIcono(
+      icono === "icono fa fa-thumbs-up"
+        ? "icono fa fa-thumbs-down"
+        : "icono fa fa-thumbs-up"
+    );
+  };
+  return (
+    <div className="App">
+      <FlipIcon nameIcon={icono} />
+      <Button name="Cambiar Icono" funcion={cambiarIcono} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Component/FlipIcon.js
@@ -0,0 +1,7 @@
+import "./flipIcon.css";
+
+function FlipIcon({ nameIcon }) {
+  return <i className={nameIcon} />;
+}
+
+export default FlipIcon;
